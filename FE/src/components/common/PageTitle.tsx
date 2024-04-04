33e643c48b6d9e8cd1d25528f0d87bee9863d827
박건규 <geongyu09@gmail.com>
2024-04-04 19:25:05 +0900

    refactor: PageTitle 컴포넌트 재사용성을 위한 props 추가
    색상 변경을 위한 props 추가.
    안정적인 조건적 tailwind 사용을 위한 cn 유틸 적용
    
    #171

--- FE/src/components/common/PageTitle.tsx
+++ FE/src/components/common/PageTitle.tsx
@@ -1,10 +1,20 @@
+import { cn } from "@/utils/cn";
+
 type Props = {
   title: string;
+  grey?: boolean;
 };
 
-export default function PageTitle({ title }: Props) {
+export default function PageTitle({ title, grey }: Props) {
   return (
-    <div className="flex items-center text-color121 text-3xl font-bold mt-11">
+    <div
+      className={cn(
+        "flex items-centertext-3xl font-bold mt-11 text-color121 ",
+        {
+          "text-color999": grey,
+        }
+      )}
+    >
       <h1 className="shrink-0 mr-[2.375rem]">{title}</h1>
       <hr className="w-full bg-colorede h-[1.5px] border-none" />
     </div>
