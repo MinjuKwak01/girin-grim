5fd6eb9278854db1f4a193bbcc54515019d857d0
MinjuKwak01 <mkwak1125@gmail.com>
2024-04-08 13:07:51 +0900

    feat : interface에 메서드 추가

--- BE/girin-grim/src/main/java/com/starshop/giringrim/funding/service/FundingService.java
+++ BE/girin-grim/src/main/java/com/starshop/giringrim/funding/service/FundingService.java
@@ -33,5 +33,9 @@ public interface FundingService {
      */
     FundingRespDtos.HomeDto home(Integer page, Long universityId, String fundingType, String keyword, String method, UserDetailsImpl userDetails);
 
+    /*
+     *   공지 작성
+     */
+    void createNotice(FundingReqDtos.NoticeDto noticeDto, Long fundingId, UserDetailsImpl userDetails);
 
 }
