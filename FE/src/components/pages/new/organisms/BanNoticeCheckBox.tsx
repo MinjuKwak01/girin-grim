0ee4547884742d91e677ceabfe00c18a0c3efc92
박건규 <geongyu09@gmail.com>
2024-05-25 20:11:04 +0900

    remove: 사용하지 않는 파일 삭제
    사용하지 않는 파일ㅇ르 삭제합니다.
    
    #171

--- FE/src/components/pages/new/organisms/BanNoticeCheckBox.tsx
+++
@@ -1,7 +0,0 @@
-import CheckBox from "../ui/CheckBox";
-
-export default function BanNoticeCheckBox() {
-  const checkLabel =
-    "위 내용을 숙지했으며 이에 위배되지 않은 품목으로 펀딩을 신청함.";
-  return <CheckBox checkLabel={checkLabel} />;
-}
